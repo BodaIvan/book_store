e93e934bce84e0983c4dbd29970b4313c98caf37
BodaIvan <ivanboda91@gmail.com>
2021-05-31 01:52:41 +0300

    add rolles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'books#index'
-  resources :books
+  resources :books, only: [:show, :index]
+  namespace :admin do
+    resources :books
+  end
 end
